866fb4c395979002be7c9d6236c8f722ea6f62b3
ShokoEnya <enyashoko@gmail.com>
2021-01-13 19:39:41 +0900

    ルーティング、コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sites/index'
   root to: "sites#index"
-  get 'sites/introduction' => 'sites#introduction'
+  get 'sites/introduction'
+  get 'sites/real_estate'
 end
