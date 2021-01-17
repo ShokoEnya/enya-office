3e3bf41958a8e67840e7d6098b75c5b8afe0596b
ShokoEnya <enyashoko@gmail.com>
2021-01-17 18:48:06 +0900

    商業登記ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sites/index'
   root to: "sites#index"
+  get 'sites/index'
   get 'sites/introduction'
   get 'sites/real_estate'
+  get 'sites/commerce'
 end
