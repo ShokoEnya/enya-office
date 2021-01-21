fbed49d085d1e74b0965832ca6fa2b986ee8105b
ShokoEnya <enyashoko@gmail.com>
2021-01-21 15:51:23 +0900

    ビューの修正

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,4 +1,6 @@
 class InformationController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @information = Information.order('created_at DESC')
   end
